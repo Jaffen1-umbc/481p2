c47c4783035ee3db10601bb63ac568bcc7a6adcc
Jaffen1-umbc <32547207+Jaffen1-umbc@users.noreply.github.com>
2018-12-01 13:37:39 -0500

    trying this for server

--- proofOfConceptServer.py
+++ proofOfConceptServer.py
@@ -3,9 +3,9 @@ from socket import *
 import sys, select, struct
 
 #SETUP UDP DATAGRAM SOCKET
-SOCK = socket(AF_INET, SOCK_DGRAM)
-TTT_SERVER_PORT = 13037
-SOCK.bind(('',TTT_SERVER_PORT))
+#SOCK = socket(AF_INET, SOCK_DGRAM)
+#TTT_SERVER_PORT = 13037
+#SOCK.bind(('',TTT_SERVER_PORT))
 
 ACTIVE_GAMES = []	#the global array to store the active games
 CLIENT_QUEUE = []
@@ -25,7 +25,7 @@ def get_active_game_index_or_none(addr):
 		
 	return None
 
-def main():
+def main2():
 	global UNIQUE_ID_COUNTER
 
 
@@ -56,6 +56,47 @@ def main():
 		#UDP? 	v.conn.close()
 		#UDP? SOCK.close()
 		sys.exit(0)
-		
+
+# Math Server
+import struct
+import socket
+
+def main():
+	udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	udp_socket.bind(("localhost", 5000))
+
+	while True:
+		# Receive and unpack the data
+		data, address = udp_socket.recvfrom(512)
+		try:
+			op, a, b = struct.unpack(">3sii", data)
+			op = op.decode('ascii')
+
+		except struct.error:
+			# Handle the case where we receive a malformed packet
+			print("Unable to unpack packet")
+
+		else:
+		# Perform the calculation
+			result = 0
+			success = 0
+		if op == "add":
+			result = a + b
+			success = 1
+		elif op == "sub":
+			result = a - b
+			success = 1
+		elif op == "mul":
+			result = a * b
+			success = 1
+		#catch the divide by zero
+		elif op == "div" and b != 0:
+			result = a // b
+			success = 1
+
+		# Pack and send the result
+		binary = struct.pack(">bi", success, result)
+		udp_socket.sendto(binary, address)
+	
 if __name__ == '__main__':
 	main()
