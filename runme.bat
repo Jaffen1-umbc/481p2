7a0dd680b21e6e4a25535ffd0d917182ec3aff91
Jaffen1-umbc <32547207+Jaffen1-umbc@users.noreply.github.com>
2019-02-28 16:28:36 -0500

    removed profanity

--- runme.bat
+++ runme.bat
@@ -1,6 +1,6 @@
-cat ./test.txt > shit1
-cat ./test.txt >> shit2
-echo `cat ./test.txt` > poop1
-echo `cat ./test.txt` >> poop2
+cat ./test.txt > s1
+cat ./test.txt >> s2
+echo `cat ./test.txt` > p1
+echo `cat ./test.txt` >> p2
 ls -ali
 
